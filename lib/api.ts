2bc0e6f9584b1addd913a05ccffbeb758e044dd7
Nikhil Kadam <nickkadam23@gmail.com>
2024-07-04 23:39:26 +0530

    feat: add axios apis

---
+++ lib/api.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
+
+export const setAuthToken = (token: string) => {
+  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
+export const login = async (username: string, password: string) => {
+  const response = await api.post("/auth/login", { username, password });
+  return response.data;
+};
+
+export const register = async (username: string, password: string) => {
+  const response = await api.post("/auth/register", { username, password });
+  return response.data;
+};
+
+export const getNotes = async () => {
+  const response = await api.get("/notes");
+  return response.data;
+};
+
+export const getNote = async (id: string) => {
+  const response = await api.get(`/notes/${id}`);
+  return response.data;
+};
+
+export const createNote = async (note: {
+  title: string;
+  body: string;
+  tags: string[];
+}) => {
+  const response = await api.post("/notes", note);
+  return response.data;
+};
+
+export const updateNote = async (
+  id: string,
+  note: { title: string; body: string; tags: string[] }
+) => {
+  const response = await api.put(`/notes/${id}`, note);
+  return response.data;
+};
+
+export const deleteNote = async (id: string) => {
+  const response = await api.delete(`/notes/${id}`);
+  return response.data;
+};
+
+export default api;
