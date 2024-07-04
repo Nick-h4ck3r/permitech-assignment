f286da4b0e35ce6b6e85536c3ae8de3c80c8fb43
Nikhil Kadam <nickkadam23@gmail.com>
2024-07-05 01:17:13 +0530

    feat: public route - redirect authenticated user

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,24 @@
 import { LoginForm } from "@/components/LoginForm";
+import { PublicRoute } from "@/components/PublicRoute";
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="">
-        <h1 className="text-2xl font-bold mb-4">Login</h1>
-        <LoginForm />
-        <p className="text-sm py-2">
-          Don't have an account?{" "}
-          <a
-            href="/register"
-            className="text-blue-500"
-          >
-            Register
-          </a>
-        </p>
+    <PublicRoute>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="">
+          <h1 className="text-2xl font-bold mb-4">Login</h1>
+          <LoginForm />
+          <p className="text-sm py-2">
+            Don't have an account?{" "}
+            <a
+              href="/register"
+              className="text-blue-500"
+            >
+              Register
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
+    </PublicRoute>
   );
 }
